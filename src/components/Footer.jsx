0f1045aa2fe7d92018f525139f8648f300cc239c
Sidd Shende <siddshende@utexas.edu>
2022-11-30 12:58:06 -0600

    final front end done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,10 @@
 import styled from "styled-components"
 import { Facebook, Instagram, Pinterest, Twitter, Room, Phone, MailOutline} from "@material-ui/icons"
+import {mobile} from "../responsive"
 
 const Container = styled.div`
 	display: flex;
+	${mobile({flexDirection: "column"})}
 `
 
 const Left = styled.div`
@@ -38,6 +40,7 @@ const SocialIcon = styled.div`
 const Center = styled.div`
 	flex: 1;
 	padding: 20px;
+	${mobile({display: "none"})}
 `
 
 const Title = styled.h3`
@@ -60,6 +63,7 @@ const ListItem = styled.li`
 const Right = styled.div`
 	flex: 1;
 	padding: 20px;
+	${mobile({backgroundColor: "#eee"})}
 `
 
 const ContactItem = styled.div`
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,17 +1,42 @@
 import styled from "styled-components";
 import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import Products from "../components/Products";
+import {mobile} from "../responsive"
 
 const Container = styled.div`
 `
 
 const Title = styled.h1`
+	margin: 20px;
 `
 
 const FilterContainer = styled.div`
+	display: flex;
+	justify-content: space-between;
 `
 
 const Filter = styled.div`
+	margin: 20px;
+	${mobile({margin: "0px 20px", display: "flex", flexDirection: "column"})}
+`
+
+const FilterText = styled.span`
+	font-size: 20px;
+	font-weight: 600;
+	margin-right: 20px;
+	${mobile({marginRight: "0px"})}
+`
+
+const Select = styled.select`
+	padding: 10px;
+	margin-right: 20px;
+	${mobile({marginRight: "10px 0px"})}
+`
+
+const Option = styled.option`	
 `
 
 const ProductList = () => {
@@ -24,12 +49,45 @@ const ProductList = () => {
 		</Title>
 		<FilterContainer>
 			<Filter>
-				Filter1
+				<FilterText>
+					Filter Products:
+				</FilterText>
+				<Select>
+					<Option disabled selected>
+						Color
+					</Option>
+					<Option>White</Option>
+					<Option>Black</Option>
+					<Option>Red</Option>
+					<Option>Blue</Option>
+					<Option>Yellow</Option>
+					<Option>Green</Option>
+				</Select>
+				<Select>
+					<Option disabled selected>
+						Size
+					</Option>
+					<Option>XS</Option>
+					<Option>S</Option>
+					<Option>M</Option>
+					<Option>L</Option>
+					<Option>XL</Option>
+				</Select>
 			</Filter>
 			<Filter>
-				Filter2
+				<FilterText>
+					Sort Products:
+				</FilterText>
+				<Select>
+					<Option selected>Newest</Option>
+					<Option>Price (asc)</Option>
+					<Option>Price (desc)</Option>
+				</Select>
 			</Filter>
 		</FilterContainer>
+		<Products/>
+		<Newsletter/>
+		<Footer/>
 	</Container>
   )
 }
